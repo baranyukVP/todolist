74f80f871e7cca23904cf9b89f39f74554187efd
baranyukVP <30409319+baranyukVP@users.noreply.github.com>
2019-07-14 15:58:35 +0300

    Added NewTask form

--- src/App.js
+++ src/App.js
@@ -4,19 +4,66 @@ import './App.css';
 import Task from './Task';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: props.cookies ?.tasks,
+            taskName: '',
+            taskDescription: ''
+        }
+    }
+
+    addNewTask(e) {
+        e.preventDefault();
+
+    }
+
+    onChangeNewTaskParams(e) {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
-        const cookies = this.props.cookies;
-        const tasks = cookies ?.tasks;
+        const {
+            tasks,
+            taskName,
+            taskDescription
+        } = this.state;
         return (
             <div className="App">
                 <h1>ToDoList</h1>
-                {tasks && tasks.map((task) => (
-                    <Task
-                        key={task.id}
-                        name={task.name}
-                        description={task.description}
-                    />)
-                )}
+                <div className="task-list">
+                    {tasks && tasks.map((task) => (
+                        <Task
+                            key={task.id}
+                            name={task.name}
+                            description={task.description}
+                        />)
+                    )}
+                    <form>
+                        <input
+                            name="taskName"
+                            value={taskName}
+                            onChange={this.onChangeNewTaskParams}
+                            onBlur={this.onChangeNewTaskParams}
+                        />
+                        <input
+                            name="taskDescription"
+                            value={taskDescription}
+                            onChange={this.onChangeNewTaskParams}
+                            onBlur={this.onChangeNewTaskParams}
+                        />
+                    </form>
+                </div>
+                <div className="task-btn-bar">
+                    <button
+                        onClick={this.addNewTask}
+                    >
+                        Add new task
+                    </button>
+                </div>
             </div>
         );
     }
