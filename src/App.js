08494f75e3444a205262ac863b5f199b8fae2b6e
baranyukVP <30409319+baranyukVP@users.noreply.github.com>
2019-07-14 15:38:21 +0300

    setting up dependencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { withCookies } from 'react-cookie';
 import './App.css';
 import Task from './Task'
 
 function App() {
+    const cookies = this.props.cookies;
+    const tasks = cookies?.tasks;
     return (
         <div className="App">
             <h1>ToDoList</h1>
-            <Task
-                name="name"
-                description="description"
-            />
+            {tasks && tasks.map((task) => (
+                <Task
+                    key={task.id}
+                    name={task.name}
+                    description={task.description}
+                />)
+            )}
         </div>
     );
 }
 
-export default App;
+export default withCookies(App);
