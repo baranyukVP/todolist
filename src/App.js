2cd81fc8d2dcf793841c794020a27575dab65faf
baranyukVP <30409319+baranyukVP@users.noreply.github.com>
2019-07-14 15:46:55 +0300

    babel config; App() -> class App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { withCookies } from 'react-cookie';
 import './App.css';
-import Task from './Task'
+import Task from './Task';
 
-function App() {
-    const cookies = this.props.cookies;
-    const tasks = cookies?.tasks;
-    return (
-        <div className="App">
-            <h1>ToDoList</h1>
-            {tasks && tasks.map((task) => (
-                <Task
-                    key={task.id}
-                    name={task.name}
-                    description={task.description}
-                />)
-            )}
-        </div>
-    );
+class App extends React.Component {
+    render() {
+        const cookies = this.props.cookies;
+        const tasks = cookies ?.tasks;
+        return (
+            <div className="App">
+                <h1>ToDoList</h1>
+                {tasks && tasks.map((task) => (
+                    <Task
+                        key={task.id}
+                        name={task.name}
+                        description={task.description}
+                    />)
+                )}
+            </div>
+        );
+    }
 }
 
 export default withCookies(App);
